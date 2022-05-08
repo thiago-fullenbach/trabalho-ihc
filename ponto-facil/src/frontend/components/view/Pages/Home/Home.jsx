d516910b49b0ce475140bf9e1ff9618f704dbec9
Thiago Carvalho Füllenbach <thiago.fullenbach@hotmail.com>
2022-05-08 19:07:24 -0300

    Adiciona página de cadastro de locais

--- ponto-facil/src/frontend/components/view/Pages/Home/Home.jsx
+++ ponto-facil/src/frontend/components/view/Pages/Home/Home.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Main from "../../../templates/Main/Main";
+import axios from 'axios';
 
-import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { faHome, faClock } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Table from "../../../templates/Table/Table";
 
 const headerProps = {
     icon: faHome,
@@ -9,12 +12,69 @@ const headerProps = {
     subtitle: "Registrar presença no trabalho e consultar pontos registrados"
 }
 
+const baseUrl = 'http://localhost:3001/presencas';
+const initialProps = {
+    list: []
+}
+
 export default props => {
+    const [presencaList, setPresencaList] = useState(initialProps.list)
+
+    useEffect(() => {
+        axios(baseUrl).then(resp => {
+            console.log(resp.data)
+            setPresencaList(resp.data)
+        })
+    }, [])
+
+    const getRowColor = status => {
+        if(status === "Aprovado") return "default"
+        if(status === "Reprovado") return "danger"
+        return "warning"
+    }
+
+    const renderRows = () => {
+        return presencaList.map(presenca => {
+            return (
+                <tr key={presenca.id}
+                    className={`table-${getRowColor(presenca.status)}`}>
+                    <td>{presenca.id}</td>
+                    <td>{presenca.tipoPresenca}</td>
+                    <td>{presenca.horaPresenca}</td>
+                    <td>{presenca.dataPresenca}</td>
+                    <td>{presenca.localPresenca}</td>
+                    <td>{presenca.status}</td>
+                    <td>
+                        <button className="btn btn-primary" tooltip="Ajustar">
+                            <FontAwesomeIcon icon={faClock} />
+                        </button>
+                    </td>
+                </tr>
+            )
+        })
+    }
+
     return (
         <Main {...headerProps} >
-                <div className="display-4">Bem Vindo, user_name!</div>
+                <div className="display-4">user_name</div>
                 <hr />
-                <p className="mb-0">Sistema para exemplificar a construção de um cadastro desenvolvido em React!</p>
+                
+                {presencaList.length > 0 ? (
+                    <Table headings={[
+                        "#",
+                        "Tipo",
+                        "Horário",
+                        "Data",
+                        "Local",
+                        "Status"
+                    ]}>
+                        {renderRows()}
+                    </Table>
+                ) : (<h3 className="d-flex justify-content-center my-5 text-secondary">Não existem registros de presença para este usuário</h3>)}
+
+                <button className="btn btn-primary">
+                    Registrar Presença
+                </button>
         </Main>
     )
 }
--- ponto-facil/src/frontend/main/Routes.jsx
+++ ponto-facil/src/frontend/main/Routes.jsx
@@ -4,14 +4,14 @@ import { Routes, Route } from 'react-router-dom';
 import Home from "../components/view/Pages/Home/Home";
 import Config from "../components/view/Pages/Config/Config";
 import EmployeeCrud from "../components/view/Pages/Employees/EmployeeCrud";
+import Locations from '../components/view/Pages/Locations/Locations';
 
 export default props => {
-
-    
     return (
         <Routes>
             <Route exact path ="/home" element={<Home />}></Route>
             <Route path="/employees" element={<EmployeeCrud />}></Route>
+            <Route path="/locations" element={<Locations />}></Route>
             <Route path="/config" element={<Config />}></Route>
             <Route exact path ="/*" element={<Home />}></Route>
         </Routes>
